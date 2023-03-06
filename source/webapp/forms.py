52443b330c58e6c36dd8a9c8984991efa54943a7
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-03-06 21:54:47 +0600

    замена валидатора формы

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,13 +1,11 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from webapp.models import Task
-from django.core.validators import MinLengthValidator, BaseValidator
-from datetime import datetime
+from django.core.validators import BaseValidator
 
 
-class CustomLenValidator(BaseValidator):
+class CustomMaxLenValidator(BaseValidator):
     def __init__(self, limit_value):
-        message = 'Максимальная длина заголовка %(limit_value)s. Выввели %(show_value)s символов'
+        message = 'Максимальная длина поля %(limit_value)s. Выввели %(show_value)s символов'
         super().__init__(limit_value=limit_value, message=message)
 
     def compare(self, value, limit_value):
@@ -17,30 +15,25 @@ class CustomLenValidator(BaseValidator):
         return len(value)
 
 
-class CustomDateValidator(BaseValidator):
-    message = 'Введите дату в формате гггг-мм-дд'
+class CustomMinLenValidator(BaseValidator):
+    def __init__(self, limit_value):
+        message = 'Минимальная длина поля %(limit_value)s. Выввели %(show_value)s символов'
+        super().__init__(limit_value=limit_value, message=message)
 
-    def __init__(self, limit_value=None):
-        super().__init__(limit_value)
+    def compare(self, value, limit_value):
+        return value < limit_value
 
-    def __call__(self, value=None):
-        if value and value.strip():
-            try:
-                datetime.strptime(value, '%Y-%m-%d')
-            except ValueError:
-                raise ValidationError(self.message, code=self.code)
+    def clean(self, value):
+        return len(value)
 
 
 class TaskForm(forms.ModelForm):
     title = forms.CharField(
         max_length=200,
-        validators=(MinLengthValidator(limit_value=2, message='Минимальная длина 2 символа'), CustomLenValidator(20)))
+        validators=(CustomMinLenValidator(2), CustomMaxLenValidator(200)))
     description = forms.CharField(
         max_length=3000,
-        validators=(MinLengthValidator(limit_value=2, message='Минимальная длина 2 символа'), CustomLenValidator(3000)))
-    completion_date = forms.CharField(
-        validators=(CustomDateValidator(),)
-    )
+        validators=(CustomMinLenValidator(2), CustomMaxLenValidator(3000)))
 
     class Meta:
         model = Task
